6933e1dbd4369fefd294dae31a67d80707a1be38
Darren <darrenalive@gmail.com>
2023-01-11 18:03:15 +1100

    fix media class white space and optional caption

--- src/component/Media/index.tsx
+++ src/component/Media/index.tsx
@@ -31,10 +31,13 @@ export const Media = ({
   <figure
     className={`nsw-media${
       left === undefined || left === "none" ? "" : ` nsw-media--left-${left}`
-    }
-  ${options[style]}
-  ${right === undefined || right === "none" ? "" : ` nsw-media--right-${right}`}
-  ${center === undefined || center === "none" ? "" : ` nsw-media--${center}`}`}
+    } ${options[style]} ${
+      right === undefined || right === "none"
+        ? ""
+        : ` nsw-media--right-${right}`
+    } ${
+      center === undefined || center === "none" ? "" : ` nsw-media--${center}`
+    }`}
   >
     {video ? (
       <div className="nsw-media__video">
@@ -44,7 +47,7 @@ export const Media = ({
       ""
     )}
     {image ? <img src={image} alt={title} /> : ""}
-    <figcaption>{caption}</figcaption>
+    {caption && <figcaption>{caption}</figcaption>}
   </figure>
 );
 
