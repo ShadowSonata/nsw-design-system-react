362520e5a573a39d0020090b8afdecc74a4c0b7e
Darren <darrenalive@gmail.com>
2023-01-23 16:26:15 +1100

    make tabs working with/without hash router

--- src/component/Tabs/index.tsx
+++ src/component/Tabs/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 export interface TabsProps {
   className?: string;
@@ -8,16 +8,18 @@ export interface TabsProps {
 //TODO: error handling when TabItems != Tabsections etc
 function Tabs({ className = "", children }: TabsProps) {
   const [tabIndex, setTabIndex] = useState(0);
-  const handleTabSelect = (i: number) => (e: React.SyntheticEvent) => {
-    e.preventDefault();
+
+  const updateTabIndex = (i: number) => {
+    // e.preventDefault()
     setTabIndex(i);
   };
+
   const childrenWithTabIndex = React.Children.map(children, (child, index) => {
     // item wrapper
     if (index === 0) {
       return React.cloneElement(child as React.ReactElement<any>, {
-        activeTab: tabIndex,
-        handleClick: handleTabSelect,
+        activeTabIndex: tabIndex,
+        updateTabIndex,
       });
     } else {
       return React.cloneElement(child as React.ReactElement<any>, {
@@ -31,22 +33,24 @@ function Tabs({ className = "", children }: TabsProps) {
 
 interface TabItemWrapperProps {
   children: React.ReactNode;
-  activeTab?: number;
-  handleClick?: (i: number) => () => void;
+  activeTabIndex?: number;
+  updateTabIndex?: (i: number) => void;
 }
 
 export const TabItemWrapper = ({
   children,
-  activeTab,
-  handleClick,
+  activeTabIndex,
+  updateTabIndex,
 }: TabItemWrapperProps) => {
   return (
     <div className="nsw-tabs__list-wrapper">
       <ul className="nsw-tabs__list" role="tablist">
         {React.Children.map(children, (child, index) =>
           React.cloneElement(child as React.ReactElement<any>, {
-            active: activeTab === index,
-            handleClick: handleClick && handleClick(index),
+            // active: activeTab === index,
+            tabIndex: index,
+            updateTabIndex,
+            activeTabIndex,
           })
         )}
       </ul>
@@ -57,37 +61,95 @@ export const TabItemWrapper = ({
 interface TabItemProps {
   urlHash?: string;
   title: string;
-  active?: boolean;
-  handleClick?: () => void;
+  tabIndex?: number;
+  activeTabIndex?: number;
+  updateTabIndex?: (i: number) => void;
 }
 
 export const TabItem = ({
   urlHash,
   title,
-  active,
-  handleClick,
-}: TabItemProps) => (
-  <li className="nsw-tabs__list-item" onClick={handleClick}>
-    <a
-      href={urlHash ? `#${urlHash}` : "#"}
-      className={`nsw-tabs__link ${active && "active"}`}
-      role="tab"
-      aria-selected={active}
+  tabIndex,
+  activeTabIndex,
+  updateTabIndex,
+}: TabItemProps) => {
+  const [hash, updateHash] = useHash();
+  const isUsingHash = !!urlHash;
+  const active =
+    isUsingHash && hash ? hash === `#${urlHash}` : tabIndex === activeTabIndex;
+
+  useEffect(() => {
+    if (active) {
+      updateTabIndex!(tabIndex!);
+    }
+  }, [active, tabIndex, updateTabIndex]);
+
+  return (
+    <li
+      className="nsw-tabs__list-item"
+      onClick={(e) => {
+        updateTabIndex!(tabIndex!);
+        if (isUsingHash) {
+          updateHash(`#${urlHash}`);
+        } else {
+          e.preventDefault();
+        }
+      }}
     >
-      {title}
-    </a>
-  </li>
-);
+      <a
+        href={urlHash ? `#${urlHash}` : ""}
+        className={`nsw-tabs__link ${active && "active"}`}
+        role="tab"
+        aria-selected={active}
+      >
+        {title}
+      </a>
+    </li>
+  );
+};
 
 interface TabSectionProps {
   children: React.ReactNode;
   active?: boolean;
 }
 
-export const TabSection = ({ children, active }: TabSectionProps) => (
-  <section className="nsw-tabs__content" hidden={!active}>
-    {children}
-  </section>
-);
+export const TabSection = ({ children, active }: TabSectionProps) => {
+  return (
+    <section className="nsw-tabs__content" hidden={!active}>
+      {children}
+    </section>
+  );
+};
+
+const useHash = () => {
+  const [hash, setHash] = React.useState<string>("");
+
+  const hashChangeHandler = React.useCallback(() => {
+    setHash(window?.location.hash);
+  }, []);
+  useEffect(() => {
+    if (window) {
+      setHash(window.location.hash);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!window) return;
+    window.addEventListener("hashchange", hashChangeHandler);
+    return () => {
+      window.removeEventListener("hashchange", hashChangeHandler);
+    };
+  }, [hashChangeHandler]);
+
+  const updateHash = useCallback(
+    (newHash: string) => {
+      if (!window) return;
+      if (newHash !== hash) window.location.hash = newHash;
+    },
+    [hash]
+  );
+
+  return [hash, updateHash] as const;
+};
 
 export default Tabs;
